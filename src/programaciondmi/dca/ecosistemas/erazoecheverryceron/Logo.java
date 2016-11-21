9d4f249971a1602cf9965cf8f7a5031ce8f6afa7
LuanErazo <luan.ep@outlook.com>
2016-11-21 01:23:22 -0500

    creacion de la clase logo

---
+++ src/programaciondmi/dca/ecosistemas/erazoecheverryceron/Logo.java
@@ -0,0 +1,18 @@
+package programaciondmi.dca.ecosistemas.erazoecheverryceron;
+
+import programaciondmi.dca.core.EcosistemaAbstracto;
+import programaciondmi.dca.core.LogoAbstracto;
+
+public class Logo extends LogoAbstracto {
+
+	public Logo(String rutaLogo, EcosistemaAbstracto ecosistema) {
+		super(rutaLogo, ecosistema);
+	}
+
+	@Override
+	public void click() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
