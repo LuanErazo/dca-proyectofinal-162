6f3cd06f49d2137d656c4b2c1805ceeeafb82c3c
Julio César <Julio Csar@DESKTOP-UPH1PD4>
2016-11-24 01:50:45 -0500

    Printeo del hijo

--- src/programaciondmi/dca/ecosistemas/erazoecheverryceron/HijoPapu.java
+++ src/programaciondmi/dca/ecosistemas/erazoecheverryceron/HijoPapu.java
@@ -1,26 +1,35 @@
 package programaciondmi.dca.ecosistemas.erazoecheverryceron;
 
+import processing.core.PConstants;
 import programaciondmi.dca.core.EcosistemaAbstracto;
 import programaciondmi.dca.core.EspecieAbstracta;
 
 public class HijoPapu extends EspeciePapu {
 
+	private int time;
+
 	public HijoPapu(EcosistemaAbstracto ecosistema) {
 		super(ecosistema);
-		
+		imagenes = CargaDatos.Bebe;
 	}
 
 	@Override
 	public void dibujar() {
-		app.fill(30,30,30);
-		app.ellipse(pos.x, pos.y, 20, 20);
+		if (app.frameCount% 2 == 0) {
+			time ++;
+			if (time >= imagenes.length) {
+				time = 0;
+			}
+		}
+		
+		app.imageMode(PConstants.CENTER);
+		app.image(imagenes[time], pos.x, pos.y);
 	}
-	
+
 	@Override
 	public boolean recibirDano(EspecieAbstracta lastimador) {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
-	
 
 }
